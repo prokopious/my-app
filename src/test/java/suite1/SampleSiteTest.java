daaa0a42abbdf1a843ff29d627e3bb3c17f5dbf5
John Smith <cirrusnite@hotmail.com>
2023-10-26 21:19:47 -0400

    'change_sel_version'

--- src/test/java/suite1/SampleSiteTest.java
+++ src/test/java/suite1/SampleSiteTest.java
@@ -8,18 +8,14 @@ import org.testng.annotations.Test;
 import util.TestBase;
 
 public class SampleSiteTest extends TestBase {
-
-
     @Test
     public void testSiteHeader2() throws InterruptedException {
-
         driver.get("http://example.com");
         testUtil.awaitLoadDOM(20);
         WebElement header = testUtil.getElement("//h1", null, 20);
         System.out.println(header.getText());
         testUtil.setHardWait(3000);
      
-
         // Assert that the header text is as expected.
 //        Assert.assertEquals(header.getText(), "Expected Header Text", "Header text does not match expected text.");
     }
